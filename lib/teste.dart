b7cbf6fa9fe76a9fdc3bee3444c04206059ae180
AlexandreCorcos <acorcos@gmail.com>
2020-10-08 01:36:55 +0100

    initial commit

---
+++ lib/teste.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContainerBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
